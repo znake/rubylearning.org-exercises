a80fdc78a4d17490da19b684c34cfe851e101d4f
Jakob Lehner <jakob.lnr@gmail.com>
2012-08-23 19:16:28 +0200

    adding exercise 4

---
+++ week1/week1_exercise4.rb
@@ -0,0 +1,4 @@
+#Exercise4. Write a Ruby program that tells you how many minutes there are in a year (do not bother right now about leap years etc.).
+
+minutes_in_one_year = 60 * 24 * 365
+puts "One year has #{minutes_in_one_year} minutes"
