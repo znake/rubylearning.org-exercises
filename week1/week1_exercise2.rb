8a8f2c6afa17bff406414d9352618dc4b6119335
Jakob Lehner <jakob.lnr@gmail.com>
2012-08-23 18:21:51 +0200

    added useful links

--- week1/week1_exercise2.rb
+++ week1/week1_exercise2.rb
@@ -1,3 +1,8 @@
 #Exercise2. Read the sprintf documentation and the % documentation in the String class and figure out the output being printed by of this Ruby code.
 
  puts "%05d" % 123 # 0 -> replace, 5 -> width, d -> digits
+
+
+ # links:
+ # http://www.ruby-doc.org/core-1.9.3/Kernel.html#method-i-sprintf
+ # http://www.ruby-doc.org/core-1.9.3/String.html#method-i-25
