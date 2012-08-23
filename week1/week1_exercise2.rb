7a1b2aa7cae855b408b5f4b8afe97ed1577664be
Jakob Lehner <jakob.lnr@gmail.com>
2012-08-23 18:21:28 +0200

    add exercise 2

---
+++ week1/week1_exercise2.rb
@@ -0,0 +1,3 @@
+#Exercise2. Read the sprintf documentation and the % documentation in the String class and figure out the output being printed by of this Ruby code.
+
+ puts "%05d" % 123 # 0 -> replace, 5 -> width, d -> digits
