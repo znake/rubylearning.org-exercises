5fac26a984a6ca6caf3ff6781b9f7fa75ab76edf
Jakob Lehner <jakob.lnr@gmail.com>
2012-08-24 11:53:17 +0200

    renamed var

--- week1/week1_exercise3.rb
+++ week1/week1_exercise3.rb
@@ -2,5 +2,5 @@
 #Note: To format the output to say 2 decimal places, we can use the Kernel's format method. For example, if x = 45.5678 then format("%.2f", x) will return the string 45.57
 
 # 60sec -> 1min, 60min -> 1h, 24h -> 1 day, 365 days -> 1 year
-age_in_seconds = 979000000.to_f / 60 / 60 / 24 / 365
-puts format("%.2f", age_in_seconds) + " years old"
+age_in_years = 979000000.to_f / 60 / 60 / 24 / 365
+puts format("%.2f", age_in_years) + " years old"
