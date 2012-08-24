f8929671cc11871975ac7ed3d80924cbd49dfd3e
Jakob Lehner <jakob.lnr@gmail.com>
2012-08-24 13:35:41 +0200

    add exercise 6

---
+++ week1/week1_exercise6.rb
@@ -0,0 +1,10 @@
+#Exercise6. Write a method called convert that takes one argument which is a temperature in degrees Fahrenheit. This method should return the temperature in degrees Celsius.
+#To format the output to say 2 decimal places, we can use the Kernel's format method. For example, if x = 45.5678 then format("%.2f", x) will return the string 45.57. Another way is to use the round function as follows:
+#puts (x*100).round/100.0
+
+def convert(fahrenheit)
+  celsius = (fahrenheit - 32).to_f * 5/9
+  format("%.2f", celsius)
+end
+
+puts convert(86)
