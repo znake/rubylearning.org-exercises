9c614f405d3d51ef046288e17bca3ee7e1031fac
Jakob Lehner <jakob.lnr@gmail.com>
2012-09-04 15:21:36 +0200

    updated methods

---
+++ week3/week3_exercise2/week3_exercise2.rb
@@ -0,0 +1,48 @@
+#Exercise2. Thanks to Marcos Souza for this exercise. A plain text file has the following contents:
+
+#test test test test test
+#test test test test test
+#test test test test test
+#test test test test test
+#test test test test test
+#test test word test test
+#test test test test test
+#test test test test test
+#test test test test test
+#test test test test test
+#test test test test test
+
+#Observe that in this file, there exists a word 'word'. Write a clever but readable Ruby program that updates this file and the final contents become like this:
+#test test test test test
+#test test test test test
+#test test test test test
+#test test test test test
+#test test test test test
+#test test inserted word test test
+#test test test test test
+#test test test test test
+#test test test test test
+#test test test test test
+#test test test test test
+
+#Do not hard-code the file name.
+
+new_lines = ""
+puts "Please enter the name of the file: "
+STDOUT.flush
+file_name = gets.chomp
+
+File.open(file_name, 'r') do |pos|
+  while line = pos.gets
+    if line.include? "word"
+      new_lines << line.sub("word", "inserted word")
+    else
+      new_lines << line
+    end
+  end
+end
+
+File.open('week3_exercise2.txt', 'w') do |pos|
+  pos.puts new_lines
+end
+
