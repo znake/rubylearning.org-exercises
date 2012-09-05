f86116276647358623c376842ef2be6f57fc95dc
Jakob Lehner <jakob.lnr@gmail.com>
2012-09-05 09:24:31 +0200

    Changed pompt to promt (fixed typo)

--- week2/Challenge 1/week2_challenge1.rb
+++ week2/Challenge 1/week2_challenge1.rb
@@ -1,6 +1,6 @@
 require 'prompt'
 
-prompt_text   = "Please insert anything:"
-anything = Promt.prompt(prompt_text)
+prompt_text = "Please insert anything:"
+anything    = Promt.prompt(prompt_text)
 
 puts anything
