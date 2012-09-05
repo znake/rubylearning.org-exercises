92359841aa0b570526c14ed6c99885149357a2ad
Jakob Lehner <jakob.lnr@gmail.com>
2012-09-05 09:27:53 +0200

    changed working directory in root directory

--- week3/week3_exercise3.rb
+++ week3/week3_exercise3.rb
@@ -1,20 +1,20 @@
 #Exercise3. Make use of the class Dir for the following -
 
 #Display your current working directory.
-working_directory = Dir.pwd
-puts working_directory
+root_directory = Dir.pwd
+puts root_directory
 
 #Create a new directory tmp under your working directory.
-Dir.mkdir(working_directory + "/tmp")
+Dir.mkdir(root_directory + "/tmp")
 
 #Change your working directory to tmp
-Dir.chdir(working_directory + "/tmp")
+Dir.chdir(root_directory + "/tmp")
 
 #Display your current working directory.
 puts Dir.pwd
 
 #Go back to your original directory.
-Dir.chdir(working_directory)
+Dir.chdir(root_directory)
 
 #Delete the tmp directory.
-Dir.delete(working_directory + "/tmp")
+Dir.delete(root_directory + "/tmp")
