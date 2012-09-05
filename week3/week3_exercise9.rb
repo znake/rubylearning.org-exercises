e857c1f54a713ae00981368165d61fc3c28970e2
Jakob Lehner <jakob.lnr@gmail.com>
2012-09-05 18:54:06 +0200

    add exercise 9

---
+++ week3/week3_exercise9.rb
@@ -0,0 +1,5 @@
+#Exercise9. Given a string, write a program to reverse the word order (rather than character order). The String and Array classes provide methods which will be very helpful for solving this exercise.
+
+s = "one two three"
+s_reversed = s.split(' ').reverse
+puts s_reversed
