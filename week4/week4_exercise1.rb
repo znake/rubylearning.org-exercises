2e9706e1a8531920ab1dd33bad8d1aa458fb6626
Jakob Lehner <jakob.lnr@gmail.com>
2012-09-26 15:46:03 +0200

    finished ex 1

--- week4/week4_exercise1.rb
+++ week4/week4_exercise1.rb
@@ -5,18 +5,25 @@ class Dog
   end
 
   def bark()
-    puts "wau wau"
+    "ruff ruff"
   end
 
   def eat()
-    puts "nom nom"
+    "nom nom"
   end
 
   def chase_cat()
-    puts "fu cat I'll get u"
+    "chasing cat"
+  end
+
+  def add_method(name, &block)
+    raise ArgumentError, "need a block" if !block
+    define_method(name, block)
   end
 
 end
 
-d = Dog.new('Pluto')
-d.bark
+d = Dog.new("Pluto")
+puts d.bark
+puts d.eat
+puts d.chase_cat
