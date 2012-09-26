52dd0df2124e3137ce72375bd26d821971b01cb2
Jakob Lehner <jakob.lnr@gmail.com>
2012-09-26 20:54:18 +0200

    refactored

--- week4/week4_exercise2.rb
+++ week4/week4_exercise2.rb
@@ -10,14 +10,12 @@ class Rectangle
   attr_reader :area, :perimeter
 
   def initialize(length, width)
-    @length    = length
-    @width     = width
-    @area      = (length * width).round(3)
-    @perimeter = 2 * (length + width).round(3)
+    @area      = (length * width)
+    @perimeter = 2 * (length + width)
   end
 
 end
 
 r = Rectangle.new(23.45, 34.67)
-puts "Area is = #{r.area}"
-puts "Perimeter is = #{r.perimeter}"
+puts "Area is = #{r.area.round(3)}"
+puts "Perimeter is = #{r.perimeter.round(3)}"
